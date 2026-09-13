c931ac2971c58b8c434a3bc9c8decbd3db580ce0
Shrivatsa Kulkarni <shrivatsa.kulkarni18.2011@gmail.com>
2026-09-13 17:32:25 +0530

    Port Black-Scholes Greeks suite to C++

---
+++ pricer/pricer.hpp
@@ -0,0 +1,64 @@
+#ifndef PRICER_HPP
+#define PRICER_HPP
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+double black_scholes_price(
+    double S,
+    double K,
+    double r, 
+    double sigma,
+    double T,
+    std::string option_type = "call" 
+);
+
+double black_scholes_delta(
+    double S,
+    double K, 
+    double r,
+    double sigma,
+    double T,
+    string option_type 
+);
+
+double black_scholes_gamma(
+    double S,
+    double K, 
+    double r,
+    double sigma,
+    double T 
+);
+
+double black_scholes_vega(
+    double S,
+    double K, 
+    double r,
+    double sigma,
+    double T 
+);
+
+double black_scholes_theta(
+    double S,
+    double K, 
+    double r,
+    double sigma,
+    double T,
+    string option_type
+);
+
+double black_scholes_rho(
+    double S,
+    double K, 
+    double r,
+    double sigma,
+    double T,
+    string option_type
+);
+
+double norm_cdf(double x);
+double norm_pdf(double x);
+
+#endif
